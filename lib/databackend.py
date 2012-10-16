dee685682f52f966c2f6b4bd8c1eba22e545a804
Lothar Braun <braun@net.in.tum.de>
2012-10-16 08:37:57 +0200

    Started oracle databackend

--- lib/databackend.py
+++ lib/databackend.py
@@ -34,11 +34,6 @@ class MySQLBackend(Backend):
 	def __init__(self, host, port, user, password, databaseName):
 		import MySQLdb
 		Backend.__init__(self, host, port, user, password, databaseName)
-		self.tableInsertCache = dict()
-		self.cachingThreshold = 10000
-		self.counter = 0
-
-		self.doCache = True
 
 	def connect(self):
 		import MySQLdb
@@ -120,8 +115,89 @@ class MySQLBackend(Backend):
 
 		self.execute(queryString)
 
+class OracleBackend(Backend):
+	def __init__(self, host, port, user, password, databaseName):
+		Backend.__init__(self, host, port, user, password, databaseName)
+
+	def connect(self):
+		import cx_Oracle
+		try:
+			connection_string = self.user + "/" + self.password + "@" + self.host + ":" + str(self.port) + "/" + self.databaseName
+			self.conn = cx_Oracle.Connection(connection_string)
+			self.cursor = cx_Oracle.Cursor(conn)
+		except Exception as inst:
+			print >> sys.stderr, "Cannot connect to Oracle database: ", inst 
+			sys.exit(1)
+
+
+	def execute(self, string):
+		import cx_Oracle
+		try: 
+			self.cursor.execute(string)
+		except (AttributeError, cx_Oracle.OperationalError):
+			self.connect()
+			self.execute(string)
+
+#	def executemany(self, string, objects):
+#		import cx_Oracle
+#		try:
+#			self.cursor.executemany(string, objects)
+#		except (AttributeError, cx_Oracle.OperationalError):
+#			self.connect()
+#			self.executemany(strin, objects)
+
+
+	def prepareCollection(self, name, fieldDict):
+		createString = "CREATE TABLE IF NOT EXISTS " + name + " ("
+		first = True
+		primary = ""
+		for field in fieldDict:
+			if not first:
+				createString += ","
+			createString += field + " " + fieldDict[field][0]
+			if fieldDict[field][1] != None:
+				primary = " PRIMARY KEY(" + field + ")"
+			first = False
+		if primary != "":
+			createString += "," + primary
+		createString += ")"
+		self.execute(createString)
+
+
+	def query(self, tablename, string):
+		string = string % (tableName)
+		self.execute(string)
+		return self.cursor.fetchall()
+
+	def insert(self, collectionName, fieldDict):
+		queryString = "MERGE INTO " + collectionName + " target USING (SELECT "
+		selectString = ""
+		matchedString = ""
+		notMatchedInsert = ""
+		notMatchedValues = ""
+		for field in fieldDict:
+			if selectString != "":
+				selectString += ","
+			if matchedString != "":
+				matchedString += ","
+			if notMatchedInsert != "":
+				notMatchedInsert += ","
+			if notMatchedValues != "":
+				notMatchedValues += ","
+			selectString += str(fieldDict[field])  + " as " + field
+			matchedString += field + "=" + str(fieldDict[field])
+			notMatchedInsert += "target." + field
+			notMatchedValues += "SOURCE." + field
+		
+		queryString += selectString + " FROM dual) SOURCE ON target
+		
+		self.execute(queryString)
+
+
 def getBackendObject(backend, host, port, user, password, databaseName):
 	if backend == "mysql":
 		return MySQLBackend(host, port, user, password, databaseName)
+	elif backend == "oracle":
+		return OracleBackend(host, port, user, password, databaseName)
 	else:
 		raise Exception("Data backend " + backend + " is not a supported backend")
