cd7166a27da89e2dcd0e465e63e770d67f7c21fa
Hadhat <boettget@net.in.tum.de>
2013-07-01 09:51:20 +0200

    IntervalAnalyzer integrated

--- analyzer/analyze.py
+++ analyzer/analyze.py
@@ -4,15 +4,23 @@
 import importer
 import analyzer
 
+print dir()
+
+
 analyzer_store = {}
 importer = importer.FlowBackendImporter()
 
 while True:
 	data = importer.getNextDataSet()
-	
-	for key, initArgs in analyzer.StatusAnalyzer.getInstances(data):
+
+	for key, initArgs in analyzer.IntervalAnalyzer.getInstances(data):
 	    if not key in analyzer_store:
-	    	analyzer_store[key] = analyzer.StatusAnalyzer(*initArgs)
+	    	analyzer_store[key] = analyzer.IntervalAnalyzer(*initArgs)
 	    analyzer_store[key].passDataSet(data)
+
+#	for key, initArgs in analyzer.StatusAnalyzer.getInstances(data):
+#	    if not key in analyzer_store:
+#	    	analyzer_store[key] = analyzer.StatusAnalyzer(*initArgs)
+#	    analyzer_store[key].passDataSet(data)
 
 
--- analyzer/analyzer/__init__.py
+++ analyzer/analyzer/__init__.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 from StatusAnalyzer import *
-
+from IntervalAnalyzer import *
